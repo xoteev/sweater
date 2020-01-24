37ef800aa7204de6856fe7b42e790d091bb6c17a
xoteev <xoteev@gmail.com>
2020-01-24 14:32:31 +0300

    связи таблиц, расширение классов

---
+++ src/main/java/com/xoteev/sweater/service/UserService.java
@@ -0,0 +1,18 @@
+package com.xoteev.sweater.service;
+
+import com.xoteev.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+        return userRepo.findByUsername(userName);
+    }
+}
