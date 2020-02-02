b1a1680d3b127526dcad80ccdc5fa319f584b47d
xoteev <xoteev@gmail.com>
2020-02-02 22:07:33 +0300

    шифрование паролей, валидация полей

--- src/main/java/com/xoteev/sweater/service/UserService.java
+++ src/main/java/com/xoteev/sweater/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
@@ -21,9 +22,19 @@ public class UserService implements UserDetailsService {
     @Autowired
     private MailSender mailSender;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
-        return userRepo.findByUsername(userName);
+        User user = userRepo.findByUsername(userName);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+
+        return user;
     }
 
     public boolean addUser(User user) {
@@ -36,6 +47,8 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         user.setActivationCode(UUID.randomUUID().toString());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userRepo.save(user);
 
         sendMessage(user);
